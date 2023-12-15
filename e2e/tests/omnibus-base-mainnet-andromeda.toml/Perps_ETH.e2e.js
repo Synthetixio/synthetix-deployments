4096f90b0ca47ff22fb3ff5945573bc047a4a10b
Noisekit <nikita@cc.snxdao.io>
2023-12-15 15:44:30 +0800

    Perps_BTC.e2e.js Perps_ETH.e2e.js

--- e2e/tests/omnibus-base-mainnet-andromeda.toml/Perps_ETH.e2e.js
+++ e2e/tests/omnibus-base-mainnet-andromeda.toml/Perps_ETH.e2e.js
@@ -14,42 +14,16 @@ const PerpsMarketProxyDeployment = require('../../deployments/PerpsMarketProxy.j
 const log = require('debug')(`e2e:${require('path').basename(__filename, '.e2e.js')}`);
 
 describe(require('path').basename(__filename, '.e2e.js'), function () {
-  const accountId = parseInt(`420${crypto.randomInt(1000)}`);
   const provider = new ethers.providers.JsonRpcProvider(
     process.env.RPC_URL || 'http://127.0.0.1:8545'
   );
-  const wallet = ethers.Wallet.createRandom().connect(provider);
   const marketId = 100;
 
   const PerpsMarketProxy = new ethers.Contract(
     PerpsMarketProxyDeployment.address,
     PerpsMarketProxyDeployment.abi,
-    wallet
+    provider
   );
-
-  it('should create new random wallet', async () => {
-    log({ wallet: wallet.address, pk: wallet.privateKey });
-    assert.ok(wallet.address);
-  });
-
-  it('should set ETH balance to 100', async () => {
-    const address = wallet.address;
-    assert.equal(await getEthBalance({ address }), 0, 'New wallet has 0 ETH balance');
-    await setEthBalance({ address, balance: 100 });
-    assert.equal(await getEthBalance({ address }), 100);
-  });
-
-  it('should create perps account', async () => {
-    assert.equal(
-      await getPerpsAccountOwner({ accountId }),
-      ethers.constants.AddressZero,
-      'New wallet should not have an account yet'
-    );
-    await createPerpsAccount({ wallet, accountId });
-    assert.equal(await getPerpsAccountOwner({ accountId }), wallet.address);
-    const permissions = await getPerpsAccountPermissions({ accountId });
-    assert.equal(permissions.length, 0);
-  });
 
   it('should have market deployed', async () => {
     const markets = await PerpsMarketProxy.getMarkets();
@@ -75,7 +49,7 @@ describe(require('path').basename(__filename, '.e2e.js'), function () {
       ethers.utils.formatEther(await PerpsMarketProxy.maxOpenInterest(marketId))
     );
     log({ maxOpenInterest });
-    assert.equal(maxOpenInterest, 525);
+    assert.equal(maxOpenInterest, 4.5);
   });
 
   it('should get market summary with ERC7412', async () => {
@@ -98,14 +72,14 @@ describe(require('path').basename(__filename, '.e2e.js'), function () {
     const { skewScale, maxFundingVelocity } = await PerpsMarketProxy.getFundingParameters(marketId);
 
     log({ skewScale, maxFundingVelocity });
-    assert.equal(Number(ethers.utils.formatEther(skewScale)), 1000000);
+    assert.equal(Number(ethers.utils.formatEther(skewScale)), 1_000_000);
     assert.equal(Number(ethers.utils.formatEther(maxFundingVelocity)), 9);
   });
 
-  it('should have 525 ETH Max Market Size', async () => {
+  it('should have 4.5 ETH Max Market Size', async () => {
     const maxSize = await PerpsMarketProxy.getMaxMarketSize(marketId);
 
-    assert.equal(ethers.utils.formatEther(maxSize), 525);
+    assert.equal(ethers.utils.formatEther(maxSize), 4.5);
   });
 
   it('should have 0.0002 Maker fee, 0.0005 Taker fee', async () => {
@@ -119,24 +93,57 @@ describe(require('path').basename(__filename, '.e2e.js'), function () {
     const params = await PerpsMarketProxy.getLiquidationParameters(marketId);
     log({ liquidationParameters: params });
 
-    assert.equal(Number(ethers.utils.formatEther(params.initialMarginRatioD18)), 1);
-    assert.equal(Number(ethers.utils.formatEther(params.minimumInitialMarginRatioD18)), 0.02);
-    assert.equal(Number(ethers.utils.formatEther(params.maintenanceMarginScalarD18)), 0.5);
-    assert.equal(Number(ethers.utils.formatEther(params.flagRewardRatioD18)), 0.01);
-    assert.equal(Number(ethers.utils.formatEther(params.minimumPositionMargin)), 0);
+    assert.equal(
+      Number(ethers.utils.formatEther(params.initialMarginRatioD18)),
+      8.92,
+      'initialMarginRatioD18'
+    );
+    assert.equal(
+      Number(ethers.utils.formatEther(params.minimumInitialMarginRatioD18)),
+      0.02,
+      'minimumInitialMarginRatioD18'
+    );
+    assert.equal(
+      Number(ethers.utils.formatEther(params.maintenanceMarginScalarD18)),
+      0.28,
+      'maintenanceMarginScalarD18'
+    );
+    assert.equal(
+      Number(ethers.utils.formatEther(params.flagRewardRatioD18)),
+      0.0003,
+      'flagRewardRatioD18'
+    );
+    assert.equal(
+      Number(ethers.utils.formatEther(params.minimumPositionMargin)),
+      50,
+      'minimumPositionMargin'
+    );
   });
 
   it('should have Max Liquidation parameters set', async () => {
     const params = await PerpsMarketProxy.getMaxLiquidationParameters(marketId);
-    log({ liquidationParameters: params });
+    log({ maxLiquidationParameters: params });
 
     assert.equal(
       Number(ethers.utils.formatEther(params.maxLiquidationLimitAccumulationMultiplier)),
-      1
+      1.5,
+      'maxLiquidationLimitAccumulationMultiplier'
     );
-    assert.equal(params.maxSecondsInLiquidationWindow.toNumber(), 30);
-    assert.equal(Number(ethers.utils.formatEther(params.maxLiquidationPd)), 0.0016);
-    assert.equal(params.endorsedLiquidator, '0x11233749514Ab8d00C0A5873DF7428b3db70030f');
+    assert.equal(
+      params.maxSecondsInLiquidationWindow.toNumber(),
+      30,
+      'maxSecondsInLiquidationWindow'
+    );
+    assert.equal(
+      Number(ethers.utils.formatEther(params.maxLiquidationPd)),
+      0.0005,
+      'maxLiquidationPd'
+    );
+    assert.equal(
+      params.endorsedLiquidator,
+      '0x11233749514Ab8d00C0A5873DF7428b3db70030f',
+      'endorsedLiquidator'
+    );
   });
 
   it('should have Max Locked OI ratio set to 0.5', async () => {
