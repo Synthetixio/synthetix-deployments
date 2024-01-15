89471bfa315ead8be8b8214fc05107c0e3a5b0e9
Noisekit <nikita@cc.snxdao.io>
2024-01-15 12:52:45 +0800

    Generate docs.md in CI

--- e2e/docgen.js
+++ e2e/docgen.js
@@ -43,8 +43,8 @@ function addrLink(chainId, address) {
   }
 }
 
-async function ownership() {
-  const log = require('debug')('e2e:ownership');
+async function contractsOwnership() {
+  const log = require('debug')('e2e:contractsOwnership');
   const provider = new ethers.providers.JsonRpcProvider(
     process.env.RPC_URL || 'http://127.0.0.1:8545'
   );
@@ -66,8 +66,6 @@ async function ownership() {
       Contract.owner().catch(() => ethers.constants.AddressZero),
       Contract.nominatedOwner().catch(() => ethers.constants.AddressZero),
     ]);
-
-    ethers.constants.AddressZero;
     log({ name, address, owner, nominatedOwner });
     out.push(
       `| ${[
@@ -80,9 +78,118 @@ async function ownership() {
   return out.join('\n');
 }
 
+async function poolsOwnership() {
+  const log = require('debug')('e2e:poolsOwnership');
+  const provider = new ethers.providers.JsonRpcProvider(
+    process.env.RPC_URL || 'http://127.0.0.1:8545'
+  );
+  const { chainId } = await provider.getNetwork();
+
+  const out = [];
+
+  out.push('| Pool ID | Pool name | Preferred | Owner | Nominated owner |');
+  out.push('| --- | --- | --- | --- | --- |');
+
+  const CoreProxyDeployment = require('./deployments/CoreProxy.json');
+
+  const CoreProxy = new ethers.Contract(
+    CoreProxyDeployment.address,
+    CoreProxyDeployment.abi,
+    provider
+  );
+  const [approvedPools, preferredPool] = await Promise.all([
+    CoreProxy.getApprovedPools().then((ids) => ids.map((id) => id.toNumber())),
+    CoreProxy.getPreferredPool().then((id) => id.toNumber()),
+  ]);
+  log({ approvedPools, preferredPool });
+  const pools = await Promise.all(
+    [{ poolId: preferredPool, isPreferred: true }]
+      .concat(approvedPools.map((poolId) => ({ poolId, isPreferred: false })))
+      .map(async ({ poolId, isPreferred }) => {
+        const [name, owner, nominatedOwner] = await Promise.all([
+          CoreProxy.getPoolName(poolId),
+          CoreProxy.getPoolOwner(poolId),
+          CoreProxy.getNominatedPoolOwner(poolId),
+        ]);
+        return { poolId, name, isPreferred, owner, nominatedOwner };
+      })
+  );
+  log({ pools });
+  for (const { poolId, name, isPreferred, owner, nominatedOwner } of pools) {
+    out.push(
+      `| ${[
+        poolId,
+        name,
+        isPreferred ? '✅' : '',
+        owner === ethers.constants.AddressZero ? 'n/a' : addrLink(chainId, owner),
+        nominatedOwner === ethers.constants.AddressZero ? 'n/a' : addrLink(chainId, nominatedOwner),
+      ].join(' | ')} |`
+    );
+  }
+
+  return out.join('\n');
+}
+
+async function marketsOwnership() {
+  const log = require('debug')('e2e:poolsOwnership');
+  const provider = new ethers.providers.JsonRpcProvider(
+    process.env.RPC_URL || 'http://127.0.0.1:8545'
+  );
+  const { chainId } = await provider.getNetwork();
+
+  const MARKET_IDS = [1];
+
+  const out = [];
+
+  out.push('| Market ID | Market name | Owner | Nominated owner |');
+  out.push('| --- | --- | --- | --- |');
+
+  const SpotMarketProxyDeployment = require('./deployments/SpotMarketProxy.json');
+
+  const SpotMarketProxy = new ethers.Contract(
+    SpotMarketProxyDeployment.address,
+    SpotMarketProxyDeployment.abi,
+    provider
+  );
+  const markets = await Promise.all(
+    MARKET_IDS.map(async (marketId) => {
+      const [name, owner, nominatedOwner] = await Promise.all([
+        SpotMarketProxy.name(marketId),
+        SpotMarketProxy.getMarketOwner(marketId),
+        // TODO: enable when we have contract view function
+        // SpotMarketProxy.getNominatedMarketOwner(marketId),
+        Promise.resolve(ethers.constants.AddressZero),
+      ]);
+      return { marketId, name, owner, nominatedOwner };
+    })
+  );
+  log({ markets });
+  for (const { marketId, name, owner, nominatedOwner } of markets) {
+    out.push(
+      `| ${[
+        marketId,
+        name,
+        owner === ethers.constants.AddressZero ? 'n/a' : addrLink(chainId, owner),
+        nominatedOwner === ethers.constants.AddressZero ? 'n/a' : addrLink(chainId, nominatedOwner),
+      ].join(' | ')} |`
+    );
+  }
+
+  return out.join('\n');
+}
+
 async function run() {
   const out = [];
-  out.push(await ownership());
+  out.push('## Contracts ownership');
+  out.push(await contractsOwnership());
+  out.push('');
+  out.push('');
+  out.push('## Pools ownership');
+  out.push(await poolsOwnership());
+  out.push('');
+  out.push('');
+  out.push('## Markets ownership');
+  out.push(await marketsOwnership());
   return out.join('\n');
 }
 
--- e2e/generateDeployments.js
+++ e2e/generateDeployments.js
@@ -2,17 +2,13 @@
 
 require('./inspect');
 
-const os = require('os');
 const path = require('path');
 const fs = require('fs/promises');
 const { ethers } = require('ethers');
-const { getMainLoader } = require('@usecannon/cli/dist/src/loader');
-const CANNON_DIRECTORY = path.join(os.homedir(), '.local', 'share', 'cannon');
 
 const fgReset = '\x1b[0m';
 const fgRed = '\x1b[31m';
 const fgGreen = '\x1b[32m';
-const fgYellow = '\x1b[33m';
 const fgCyan = '\x1b[36m';
 
 const ERC20Abi = [
@@ -37,12 +33,12 @@ const ERC20Abi = [
   'function transferOwnership(address newOwner)',
 ];
 
-const [buildLog] = process.argv.slice(2);
-if (!buildLog) {
+const [cannonState] = process.argv.slice(2);
+if (!cannonState) {
   console.error(`${fgRed}ERROR: Expected 1 argument${fgReset}`);
-  console.error(`Usage: ${fgGreen}node e2e/fetch-deployments.js ${fgCyan}buildLog${fgReset}`);
+  console.error(`Usage: ${fgGreen}node e2e/generateDeployments ${fgCyan}cannonState${fgReset}`);
   console.error(
-    `Example: ${fgGreen}node e2e/fetch-deployments.js ${fgCyan}e2e/cannon-build.log${fgReset}`
+    `Example: ${fgGreen}node e2e/generateDeployments ${fgCyan}/tmp/cannonState.json${fgReset}`
   );
   process.exit(1);
 }
@@ -52,25 +48,8 @@ function readableAbi(abi) {
 }
 
 async function run() {
-  console.log('Resolving Deployment Data URL from log', { buildLog });
-  const log = await fs.readFile(buildLog, 'utf8');
-
-  const [, url] = log.match(/Deployment Data\s*│\s*(ipfs:\/\/Qm\S+)/im) || [];
-  console.log(`Resolved URL:`, { url });
-
-  const loader = getMainLoader({
-    ipfsUrl: 'https://ipfs.synthetix.io',
-    //    ipfsUrl: 'http://127.0.0.1:5001',
-    cannonDirectory: CANNON_DIRECTORY,
-  });
-  const deployments = await loader.ipfs.read(url);
-
-  //  const hash = CliLoader.getCacheHash(url);
-  //  console.log(`Resolved FileName hash:`, { hash });
-
-  //  const deploymentsFile = `${CANNON_DIRECTORY}/ipfs_cache/${CliLoader.getCacheHash(url)}.json`;
-  //  console.log(`Deployments state file:`, { file: deploymentsFile.replace(os.homedir(), '~') });
-  //  const deployments = JSON.parse(await fs.readFile(deploymentsFile, 'utf8'));
+  console.log(`path.resolve(cannonState)`, path.resolve(cannonState));
+  const deployments = require(path.resolve(cannonState));
 
   await fs.rm(`${__dirname}/deployments`, { recursive: true, force: true });
   await fs.mkdir(`${__dirname}/deployments`, { recursive: true });
