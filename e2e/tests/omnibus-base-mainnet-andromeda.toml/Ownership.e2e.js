f896bbb48873b8cea1b39ffb19e09c3129d2ae78
cavalier_eth <91091124+cavalier-eth@users.noreply.github.com>
2024-01-15 15:44:12 +1300

    bumping version due to config change (#112)

--- e2e/tests/omnibus-base-mainnet-andromeda.toml/Ownership.e2e.js
+++ e2e/tests/omnibus-base-mainnet-andromeda.toml/Ownership.e2e.js
@@ -5,6 +5,7 @@ require('../../inspect');
 const log = require('debug')(`e2e:${require('path').basename(__filename, '.e2e.js')}`);
 
 const CoreProxyDeployment = require('../../deployments/CoreProxy.json');
+const SpotMarketProxyDeployment = require('../../deployments/SpotMarketProxy.json');
 const meta = require('../../deployments/meta.json');
 
 const MAINNET_DEPLOYER = '0xEde8a407913A874Dd7e3d5B731AFcA135D30375E';
@@ -12,12 +13,10 @@ const DAO_GNOSIS_SAFE = '0xbb63CA5554dc4CcaCa4EDd6ECC2837d5EFe83C82';
 
 // While we develop rapidly,
 // we want to quickly deploy, and keep testnet deployer as the owner temporarily
-// TODO: switch to DAO_GNOSIS_SAFE when stable
-const OWNER_ADDRESS = MAINNET_DEPLOYER;
+const OWNER_ADDRESS = DAO_GNOSIS_SAFE;
 
-// TODO: switch to ethers.constants.AddressZero when we fully transfer
-const NOMINATED_OWNER = DAO_GNOSIS_SAFE;
-//const NOMINATED_OWNER = ethers.constants.AddressZero
+//const NOMINATED_OWNER = DAO_GNOSIS_SAFE;
+const NOMINATED_OWNER = ethers.constants.AddressZero;
 
 describe(require('path').basename(__filename, '.e2e.js'), function () {
   const provider = new ethers.providers.JsonRpcProvider(
@@ -150,5 +149,26 @@ describe(require('path').basename(__filename, '.e2e.js'), function () {
         nominatedOwner: NOMINATED_OWNER,
       },
     });
+  });
+
+  it('should validate spot markets owned by DAO Safe', async () => {
+    const SpotMarketProxy = new ethers.Contract(
+      SpotMarketProxyDeployment.address,
+      SpotMarketProxyDeployment.abi,
+      provider
+    );
+    const marketId = 1;
+    const owner = await SpotMarketProxy.getMarketOwner(marketId);
+    // TODO: need a view function on contract that does not exist
+    // const nominatedOwner = await SpotMarketProxy.getNominatedMarketOwner(marketId);
+    log({ marketId, owner });
+    assert.deepEqual(
+      { marketId, owner },
+      {
+        marketId,
+        owner: OWNER_ADDRESS,
+        // nominatedOwner: NOMINATED_OWNER,
+      }
+    );
   });
 });
