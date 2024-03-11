e1d9d979be78731858bfa735996a78608e467592
Noisekit <nikita@cc.snxdao.io>
2024-03-11 10:03:39 +0800

    Remove Eth Goerli as well

--- e2e/docgen/lib/addrLink.js
+++ e2e/docgen/lib/addrLink.js
@@ -2,12 +2,12 @@ function addrLink(chainId, address) {
   switch (chainId) {
     case 1:
       return `[${address}](https://etherscan.io/address/${address})`;
-    case 5:
-      return `[${address}](https://goerli.etherscan.io/address/${address})`;
     case 11155111:
       return `[${address}](https://sepolia.etherscan.io/address/${address})`;
     case 10:
       return `[${address}](https://optimistic.etherscan.io/address/${address})`;
+    case 11155420:
+      return `[${address}](https://sepolia-optimistic.etherscan.io/address/${address})`;
     case 80001:
       return `[${address}](https://mumbai.polygonscan.com/address/${address})`;
     case 8453:
@@ -21,12 +21,12 @@ function addrHtmlLink(chainId, address) {
   switch (chainId) {
     case 1:
       return `<a href="https://etherscan.io/address/${address}"><code>${address}</code></a>`;
-    case 5:
-      return `<a href="https://goerli.etherscan.io/address/${address}"><code>${address}</code></a>`;
     case 11155111:
       return `<a href="https://sepolia.etherscan.io/address/${address}"><code>${address}</code></a>`;
     case 10:
       return `<a href="https://optimistic.etherscan.io/address/${address}"><code>${address}</code></a>`;
+    case 11155420:
+      return `<a href="https://sepolia-optimistic.etherscan.io/address/${address}"><code>${address}</code></a>`;
     case 80001:
       return `<a href="https://mumbai.polygonscan.com/address/${address}"><code>${address}</code></a>`;
     case 8453:
