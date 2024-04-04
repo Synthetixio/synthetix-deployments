3cc452f15d3527e4c465982abf4e10bfabe62e82
Sunny Vempati <5723490+sunnyvempati@users.noreply.github.com>
2024-04-04 10:47:11 -0500

    fix tests

--- e2e/tests/omnibus-base-sepolia-andromeda.toml/Liquidity_Provider.e2e.js
+++ e2e/tests/omnibus-base-sepolia-andromeda.toml/Liquidity_Provider.e2e.js
@@ -209,6 +209,21 @@ describe(require('path').basename(__filename, '.e2e.js'), function () {
       marketId: 200,
       settlementStrategyId: require('../../deployments/extras.json').btc_pyth_settlement_strategy,
     });
+    await doPriceUpdate({
+      wallet,
+      marketId: 300,
+      settlementStrategyId: require('../../deployments/extras.json').snx_pyth_settlement_strategy,
+    });
+    await doPriceUpdate({
+      wallet,
+      marketId: 400,
+      settlementStrategyId: require('../../deployments/extras.json').sol_pyth_settlement_strategy,
+    });
+    await doPriceUpdate({
+      wallet,
+      marketId: 500,
+      settlementStrategyId: require('../../deployments/extras.json').wif_pyth_settlement_strategy,
+    });
   });
 
   it(`should delegate ${SYNTH_USDC_MAX_MARKET_COLLATERAL - 200_000} sUSDC into the Spartan Council pool`, async () => {
