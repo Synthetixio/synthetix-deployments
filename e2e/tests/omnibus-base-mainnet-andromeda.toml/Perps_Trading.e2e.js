f215b5f17283f2f1e7d475bcfde4165e96551d7c
Sunny Vempati <5723490+sunnyvempati@users.noreply.github.com>
2024-04-05 10:25:40 -0500

    bump version (#177)
    * bump version
    
    * fix test

--- e2e/tests/omnibus-base-mainnet-andromeda.toml/Perps_Trading.e2e.js
+++ e2e/tests/omnibus-base-mainnet-andromeda.toml/Perps_Trading.e2e.js
@@ -131,6 +131,26 @@ describe(require('path').basename(__filename, '.e2e.js'), function () {
       marketId: 200,
       settlementStrategyId: require('../../deployments/extras.json').btc_pyth_settlement_strategy,
     });
+    await doPriceUpdate({
+      wallet,
+      marketId: 300,
+      settlementStrategyId: require('../../deployments/extras.json').snx_pyth_settlement_strategy,
+    });
+    await doPriceUpdate({
+      wallet,
+      marketId: 400,
+      settlementStrategyId: require('../../deployments/extras.json').sol_pyth_settlement_strategy,
+    });
+    await doPriceUpdate({
+      wallet,
+      marketId: 500,
+      settlementStrategyId: require('../../deployments/extras.json').wif_pyth_settlement_strategy,
+    });
+    await doPriceUpdate({
+      wallet,
+      marketId: 600,
+      settlementStrategyId: require('../../deployments/extras.json').w_pyth_settlement_strategy,
+    });
   });
 
   it('should increase max collateral for the test to 1_000_000_000_000', async () => {
