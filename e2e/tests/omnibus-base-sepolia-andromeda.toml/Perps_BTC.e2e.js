ed7abae82666d1afce611b824631a973d13b1a40
kaleb <62237347+kaleb-keny@users.noreply.github.com>
2024-04-17 11:06:34 +0200

    aligns sepolia with mainnet parameters (#198)
    * aligns sepolia with mainnet parameters
    
    * adds back endorsed
    
    * update sizes
    
    * update mmv
    
    * update more oi limits
    
    * make it pretty

--- e2e/tests/omnibus-base-sepolia-andromeda.toml/Perps_BTC.e2e.js
+++ e2e/tests/omnibus-base-sepolia-andromeda.toml/Perps_BTC.e2e.js
@@ -73,12 +73,12 @@ describe(require('path').basename(__filename, '.e2e.js'), function () {
     assert.equal(market.marketId, marketId);
   });
 
-  it('should have max open interest 300 BTC', async () => {
+  it('should have max open interest 149 BTC', async () => {
     const maxOpenInterest = parseFloat(
       ethers.utils.formatEther(await PerpsMarketProxy.maxOpenInterest(marketId))
     );
     log({ maxOpenInterest });
-    assert.equal(maxOpenInterest, 300);
+    assert.equal(maxOpenInterest, 149);
   });
 
   it('should make a price update', async () => {
@@ -109,16 +109,16 @@ describe(require('path').basename(__filename, '.e2e.js'), function () {
     assert.equal(Number(ethers.utils.formatEther(maxFundingVelocity)), 9, 'maxFundingVelocity');
   });
 
-  it('should have 300 BTC Max Market Size', async () => {
+  it('should have 149 BTC Max Market Size', async () => {
     const maxSize = await PerpsMarketProxy.getMaxMarketSize(marketId);
 
-    assert.equal(ethers.utils.formatEther(maxSize), 300);
+    assert.equal(ethers.utils.formatEther(maxSize), 149);
   });
 
-  it('should have 18M USD Max Market Value', async () => {
+  it('should have 5M USD Max Market Value', async () => {
     const maxMarketValue = await PerpsMarketProxy.getMaxMarketValue(marketId);
 
-    assert.equal(ethers.utils.formatEther(maxMarketValue), 18_000_000);
+    assert.equal(ethers.utils.formatEther(maxMarketValue), 5_000_000);
   });
 
   it('should have 0.000001 Maker fee, 0.0005 Taker fee', async () => {
