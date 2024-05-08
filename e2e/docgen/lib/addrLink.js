f2b7e97ca00a6aa1927f97a281c175ddb4b71d84
Noisekit <nikita@cc.snxdao.io>
2024-05-08 23:02:41 +1000

    Use viem chains to get explorer url for address links

--- e2e/docgen/lib/addrLink.js
+++ e2e/docgen/lib/addrLink.js
@@ -1,39 +1,14 @@
+const { extractChain } = require('viem');
+const chains = require('viem/chains');
+
 function addrLink(chainId, address) {
-  switch (Number(chainId)) {
-    case 1:
-      return `[${address}](https://etherscan.io/address/${address})`;
-    case 11155111:
-      return `[${address}](https://sepolia.etherscan.io/address/${address})`;
-    case 10:
-      return `[${address}](https://optimistic.etherscan.io/address/${address})`;
-    case 11155420:
-      return `[${address}](https://sepolia-optimistic.etherscan.io/address/${address})`;
-    case 80001:
-      return `[${address}](https://mumbai.polygonscan.com/address/${address})`;
-    case 8453:
-      return `[${address}](https://basescan.org/address/${address})`;
-    case 84532:
-      return `[${address}](https://sepolia.basescan.org/address/${address})`;
-  }
+  const chain = extractChain({ chains: Object.values(chains), id: Number(chainId) });
+  return `[${address}](${chain.blockExplorers.default.url}/address/${address})`;
 }
 
 function addrHtmlLink(chainId, address) {
-  switch (Number(chainId)) {
-    case 1:
-      return `<a href="https://etherscan.io/address/${address}"><code>${address}</code></a>`;
-    case 11155111:
-      return `<a href="https://sepolia.etherscan.io/address/${address}"><code>${address}</code></a>`;
-    case 10:
-      return `<a href="https://optimistic.etherscan.io/address/${address}"><code>${address}</code></a>`;
-    case 11155420:
-      return `<a href="https://sepolia-optimistic.etherscan.io/address/${address}"><code>${address}</code></a>`;
-    case 80001:
-      return `<a href="https://mumbai.polygonscan.com/address/${address}"><code>${address}</code></a>`;
-    case 8453:
-      return `<a href="https://basescan.org/address/${address}"><code>${address}</code></a>`;
-    case 84532:
-      return `<a href="https://sepolia.basescan.org/address/${address}"><code>${address}</code></a>`;
-  }
+  const chain = extractChain({ chains: Object.values(chains), id: Number(chainId) });
+  return `<a href="${chain.blockExplorers.default.url}/address/${address}"><code>${address}</code></a>`;
 }
 
 module.exports = {
