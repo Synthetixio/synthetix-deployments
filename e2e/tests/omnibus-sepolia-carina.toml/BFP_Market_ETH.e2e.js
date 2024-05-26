d1eabb351c0cb74723fe7c072d8abe40c16f7e21
Noisekit <noisekit@proton.me>
2024-05-26 20:16:34 +1000

    Add sepolia-carina tests and generators to CI

---
+++ e2e/tests/omnibus-sepolia-carina.toml/BFP_Market_ETH.e2e.js
@@ -0,0 +1,48 @@
+const crypto = require('crypto');
+const assert = require('assert');
+const { ethers } = require('ethers');
+require('../../inspect');
+const log = require('debug')(`e2e:${require('path').basename(__filename, '.e2e.js')}`);
+
+const { syncTime } = require('../../tasks/syncTime');
+const { getEthBalance } = require('../../tasks/getEthBalance');
+const { setEthBalance } = require('../../tasks/setEthBalance');
+
+describe(require('path').basename(__filename, '.e2e.js'), function () {
+  const provider = new ethers.providers.JsonRpcProvider(
+    process.env.RPC_URL || 'http://127.0.0.1:8545'
+  );
+  // const accountId = parseInt(`1337${crypto.randomInt(1000)}`);
+  const wallet = ethers.Wallet.createRandom().connect(provider);
+  // const wallet = new ethers.Wallet('0xab', provider);
+  // const accountId = 1337;
+  const address = wallet.address;
+  // const privateKey = wallet.privateKey;
+
+  let snapshot;
+
+  before('Create snapshot', async () => {
+    snapshot = await provider.send('evm_snapshot', []);
+    log('Create snapshot', { snapshot });
+  });
+
+  after('Restore snapshot', async () => {
+    log('Restore snapshot', { snapshot });
+    await provider.send('evm_revert', [snapshot]);
+  });
+
+  it('should sync time of the fork', async () => {
+    await syncTime();
+  });
+
+  it('should create new random wallet', async () => {
+    log({ wallet: wallet.address, pk: wallet.privateKey });
+    assert.ok(wallet.address);
+  });
+
+  it('should set ETH balance to 100', async () => {
+    assert.equal(await getEthBalance({ address }), 0, 'New wallet has 0 ETH balance');
+    await setEthBalance({ address, balance: 100 });
+    assert.equal(await getEthBalance({ address }), 100);
+  });
+});
