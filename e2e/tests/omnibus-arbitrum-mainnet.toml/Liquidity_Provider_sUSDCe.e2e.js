c18f345e39543c8acda04d12ec9be5a1af2b201a
Noisekit <noisekit@proton.me>
2024-05-28 15:49:48 +1000

    SIP-371: Add sUSDe LP collateral support to Synthetix V3 on Arbitrum

---
+++ e2e/tests/omnibus-arbitrum-mainnet.toml/Liquidity_Provider_sUSDCe.e2e.js
@@ -0,0 +1,209 @@
+const crypto = require('crypto');
+const assert = require('assert');
+const { ethers } = require('ethers');
+require('../../inspect');
+const log = require('debug')(`e2e:${require('path').basename(__filename, '.e2e.js')}`);
+
+const { getEthBalance } = require('../../tasks/getEthBalance');
+const { setEthBalance } = require('../../tasks/setEthBalance');
+const { getAccountOwner } = require('../../tasks/getAccountOwner');
+const { createAccount } = require('../../tasks/createAccount');
+const { getCollateralBalance } = require('../../tasks/getCollateralBalance');
+const { getAccountCollateral } = require('../../tasks/getAccountCollateral');
+const { isCollateralApproved } = require('../../tasks/isCollateralApproved');
+const { approveCollateral } = require('../../tasks/approveCollateral');
+const { depositCollateral } = require('../../tasks/depositCollateral');
+const { delegateCollateral } = require('../../tasks/delegateCollateral');
+const { syncTime } = require('../../tasks/syncTime');
+const { doPriceUpdateForPyth } = require('../../tasks/doPriceUpdateForPyth');
+const { setTokenBalance } = require('../../tasks/setTokenBalance');
+const { borrowUsd } = require('../../tasks/borrowUsd');
+const { withdrawCollateral } = require('../../tasks/withdrawCollateral');
+const { setConfigUint } = require('../../tasks/setConfigUint');
+const { getConfigUint } = require('../../tasks/getConfigUint');
+
+describe(require('path').basename(__filename, '.e2e.js'), function () {
+  const accountId = parseInt(`1337${crypto.randomInt(1000)}`);
+  const provider = new ethers.providers.JsonRpcProvider(
+    process.env.RPC_URL || 'http://127.0.0.1:8545'
+  );
+  const wallet = ethers.Wallet.createRandom().connect(provider);
+  const address = wallet.address;
+  const privateKey = wallet.privateKey;
+
+  let snapshot;
+  before('Create snapshot', async () => {
+    snapshot = await provider.send('evm_snapshot', []);
+    log('Create snapshot', { snapshot });
+  });
+  after('Restore snapshot', async () => {
+    log('Restore snapshot', { snapshot });
+    await provider.send('evm_revert', [snapshot]);
+  });
+
+  it('should sync time of the fork', async () => {
+    await syncTime();
+  });
+
+  it('should disable withdrawal timeout', async () => {
+    await setConfigUint({ key: 'accountTimeoutWithdraw', value: 0 });
+    assert.equal(await getConfigUint('accountTimeoutWithdraw'), 0);
+  });
+
+  it('should create new random wallet', async () => {
+    log({ wallet: wallet.address, pk: wallet.privateKey });
+    assert.ok(wallet.address);
+  });
+
+  it('should set ETH balance to 100', async () => {
+    assert.equal(await getEthBalance({ address }), 0, 'New wallet has 0 ETH balance');
+    await setEthBalance({ address, balance: 100 });
+    assert.equal(await getEthBalance({ address }), 100);
+  });
+
+  it('should make a price update', async () => {
+    await doPriceUpdateForPyth({
+      wallet,
+      feedId: require('../../deployments/extras.json').pyth_feed_id_eth,
+      priceVerificationContract: require('../../deployments/extras.json')
+        .pyth_price_verification_address,
+    });
+    await doPriceUpdateForPyth({
+      wallet,
+      feedId: require('../../deployments/extras.json').pyth_feed_id_susdc,
+      priceVerificationContract: require('../../deployments/extras.json')
+        .pyth_price_verification_address,
+    });
+    await doPriceUpdateForPyth({
+      wallet,
+      feedId: require('../../deployments/extras.json').pyth_feed_id_dai,
+      priceVerificationContract: require('../../deployments/extras.json')
+        .pyth_price_verification_address,
+    });
+    await doPriceUpdateForPyth({
+      wallet,
+      feedId: require('../../deployments/extras.json').pyth_feed_id_usdc,
+      priceVerificationContract: require('../../deployments/extras.json')
+        .pyth_price_verification_address,
+    });
+  });
+
+  it('should create user account', async () => {
+    assert.equal(
+      await getAccountOwner({ accountId }),
+      ethers.constants.AddressZero,
+      'New wallet should not have an account yet'
+    );
+    await createAccount({ wallet, accountId });
+    assert.equal(await getAccountOwner({ accountId }), address);
+  });
+
+  it('should set sUSDCe balance to 1000', async () => {
+    assert.equal(
+      await getCollateralBalance({ address, symbol: 'sUSDCe' }),
+      0,
+      'New wallet has 0 sUSDCe balance'
+    );
+    await setTokenBalance({
+      wallet,
+      balance: 1000,
+      tokenAddress: require('../../deployments/extras.json').susdc_address,
+      friendlyWhale: '0xf3fc178157fb3c87548baa86f9d24ba38e649b58',
+    });
+    assert.equal(await getCollateralBalance({ address, symbol: 'sUSDCe' }), 1000);
+  });
+
+  it('should approve sUSDCe spending for CoreProxy', async () => {
+    assert.equal(
+      await isCollateralApproved({
+        address,
+        symbol: 'sUSDCe',
+        spenderAddress: require('../../deployments/CoreProxy.json').address,
+      }),
+      false,
+      'New wallet has not allowed CoreProxy sUSDCe spending'
+    );
+    await approveCollateral({
+      privateKey,
+      symbol: 'sUSDCe',
+      spenderAddress: require('../../deployments/CoreProxy.json').address,
+    });
+    assert.equal(
+      await isCollateralApproved({
+        address,
+        symbol: 'sUSDCe',
+        spenderAddress: require('../../deployments/CoreProxy.json').address,
+      }),
+      true
+    );
+  });
+
+  it('should deposit 500 sUSDCe into the system', async () => {
+    assert.equal(await getCollateralBalance({ address, symbol: 'sUSDCe' }), 1000);
+    assert.deepEqual(await getAccountCollateral({ accountId, symbol: 'sUSDCe' }), {
+      totalDeposited: 0,
+      totalAssigned: 0,
+      totalLocked: 0,
+    });
+
+    await depositCollateral({ privateKey, symbol: 'sUSDCe', accountId, amount: 500 });
+
+    assert.equal(await getCollateralBalance({ address, symbol: 'sUSDCe' }), 500);
+    assert.deepEqual(await getAccountCollateral({ accountId, symbol: 'sUSDCe' }), {
+      totalDeposited: 500,
+      totalAssigned: 0,
+      totalLocked: 0,
+    });
+  });
+
+  it('should delegate 500 sUSDCe into the Spartan Council pool', async () => {
+    assert.deepEqual(await getAccountCollateral({ accountId, symbol: 'sUSDCe' }), {
+      totalDeposited: 500,
+      totalAssigned: 0,
+      totalLocked: 0,
+    });
+    await delegateCollateral({
+      privateKey,
+      symbol: 'sUSDCe',
+      accountId,
+      amount: 500,
+      poolId: 1,
+    });
+    assert.deepEqual(await getAccountCollateral({ accountId, symbol: 'sUSDCe' }), {
+      totalDeposited: 500,
+      totalAssigned: 500,
+      totalLocked: 0,
+    });
+  });
+
+  it('should borrow 100 USDh', async () => {
+    assert.deepEqual(await getAccountCollateral({ accountId, symbol: 'USDh' }), {
+      totalDeposited: 0,
+      totalAssigned: 0,
+      totalLocked: 0,
+    });
+    await borrowUsd({
+      wallet,
+      accountId,
+      symbol: 'sUSDCe',
+      amount: 100,
+      poolId: 1,
+    });
+    assert.deepEqual(await getAccountCollateral({ accountId, symbol: 'USDh' }), {
+      totalDeposited: 100,
+      totalAssigned: 0,
+      totalLocked: 0,
+    });
+  });
+
+  it('should withdraw borrowed 100 USDh', async () => {
+    assert.equal(await getCollateralBalance({ address, symbol: 'USDh' }), 0);
+    await withdrawCollateral({
+      privateKey,
+      accountId,
+      amount: 100,
+      symbol: 'USDh',
+    });
+    assert.equal(await getCollateralBalance({ address, symbol: 'USDh' }), 100);
+  });
+});
