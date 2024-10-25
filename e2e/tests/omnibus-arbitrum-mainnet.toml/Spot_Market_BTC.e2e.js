daf98c091fff50ae4cd3a9f7647cfb1ccdbea822
Noisekit <noisekit@proton.me>
2024-10-26 00:32:20 +1100

    fix Spot_Market_BTC.e2e.js

--- e2e/tests/omnibus-arbitrum-mainnet.toml/Spot_Market_BTC.e2e.js
+++ e2e/tests/omnibus-arbitrum-mainnet.toml/Spot_Market_BTC.e2e.js
@@ -102,7 +102,7 @@ describe(require('path').basename(__filename, '.e2e.js'), function () {
   it(`should do strict price update`, async () => {
     await doPriceUpdateForPyth({
       wallet,
-      feedId: require('../../deployments/extras.json').pyth_feed_id_btc,
+      feedId: require('../../deployments/extras.json').pyth_feed_id_tbtc,
       priceVerificationContract: require('../../deployments/extras.json')
         .pyth_price_verification_address,
     });
