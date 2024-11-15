3255657d1162c5733538f7a82e5f336a6fc365ba
Noisekit <noisekit@proton.me>
2024-11-15 14:18:08 +1100

    Rebalance pool

--- e2e/tests/omnibus-arbitrum-mainnet.toml/bootstrap.js
+++ e2e/tests/omnibus-arbitrum-mainnet.toml/bootstrap.js
@@ -9,32 +9,32 @@ Promise.resolve()
     return ensurePoolConfiguration([
       {
         marketId: extras.perps_super_market_id,
-        weightD18: 95,
+        weightD18: 50,
         maxDebtShareValueD18: ethers.utils.parseEther('1'),
       },
       {
         marketId: extras.synth_usdc_market_id,
-        weightD18: 1,
+        weightD18: 10,
         maxDebtShareValueD18: ethers.utils.parseEther('1'),
       },
       {
         marketId: extras.synth_btc_market_id,
-        weightD18: 1,
+        weightD18: 10,
         maxDebtShareValueD18: ethers.utils.parseEther('1'),
       },
       {
         marketId: extras.synth_eth_market_id,
-        weightD18: 1,
+        weightD18: 10,
         maxDebtShareValueD18: ethers.utils.parseEther('1'),
       },
       {
         marketId: extras.synth_usde_market_id,
-        weightD18: 1,
+        weightD18: 10,
         maxDebtShareValueD18: ethers.utils.parseEther('1'),
       },
       {
         marketId: extras.synth_sol_market_id,
-        weightD18: 1,
+        weightD18: 10,
         maxDebtShareValueD18: ethers.utils.parseEther('1'),
       },
     ]);
