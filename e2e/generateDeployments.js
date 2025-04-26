16a6b2e36f0f263427aafd9dc44f162c66d41722
Noisekit <noisekit@proton.me>
2025-04-26 20:23:06 +1000

    Export TreasuryStakingRewards contract deployment

--- e2e/generateDeployments.js
+++ e2e/generateDeployments.js
@@ -840,6 +840,9 @@ async function run() {
     deployments?.state?.['provision.treasury_market']?.artifacts?.imports?.treasury_market;
   if (treasuryMarket) {
     contracts.TreasuryMarketProxy = treasuryMarket.contracts.Proxy;
+    if (treasuryMarket.contracts.Staking) {
+      contracts.TreasuryStakingRewards = treasuryMarket.contracts.Staking;
+    }
     Object.assign(extras, {
       treasury_market_id: treasuryMarket?.extras?.marketId,
     });
